2ab044e92ca4880bd6a104b80c7556b68d644a1e
github username <github email address>
2018-10-10 16:24:28 -0400

    event listener for a sequence of events.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,31 @@ const codes = [
   "a"
 ];
 
+// // Keys for A, B, and C keys.
+// const alphabet = ['a', 'b', 'c'];
+
+// Keep track of index outside of the event handler.
+
+
 function init() {
   // your code here
+  let index = 0;
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key;
+
+    if (key === codes[index])
+    {
+      index++;
+
+      if (index === codes.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    }
+    else
+    {
+      index = 0;
+    }
+  });
 }
